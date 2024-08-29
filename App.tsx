a0caea259d7747e9a7ea687b1086a33163f9526b
giuliana <gralhagiuliana@gmail.com>
2024-08-29 10:18:44 -0300

    CRU - Arquivo de configurações, dados, recursos, parâmetros

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>SO BORA COM ESSE PROJETO</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
