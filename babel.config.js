491e315d2b8bf58728940219fa459d50c5225b36
LarissaSL <larissa.silvaedge@gmail.com>
2024-10-14 20:34:41 -0300

    FIX - Configuracoes para as Variaveis de Ambiente

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', '@babel/preset-react'],
+    presets: [
+      'babel-preset-expo',
+      '@babel/preset-react'
+    ],
+    plugins: [
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      }]
+    ]
   };
 };
-
--- src/service/api.ts
+++ src/service/api.ts
@@ -1,10 +1,8 @@
-import axios from "axios";
+import { API_URL } from '@env';
+import axios from 'axios';
 
 const api = axios.create({
-    //endereço temporario do ngrok, lembrar de gerar e pegar o novo endereço
-    baseURL: "http://192.168.10.3:8080"
-    // baseURL:"http://seu-IP:8080"
-})
+  baseURL: API_URL,
+});
 
 export default api;
-
