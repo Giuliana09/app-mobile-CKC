8f2ac405a8cd3db3d7ddd597fde14bdf58c325da
SilvanaMenezes <silvanasales.menezes@gmail.com>
2024-11-17 23:53:19 -0300

    STYLE - estilizando a tela perfil

--- src/tabs/Perfil.tsx
+++ src/tabs/Perfil.tsx
@@ -1,113 +1,138 @@
-import { VStack, Text } from "native-base";
-import React, { useEffect, useState } from 'react';
-import { View, TextInput, Button, Alert } from 'react-native';
-import { useRoute, RouteProp, useNavigation } from '@react-navigation/native';
-import { listarDadosParaCheckIn, processarCheckIn, verificarSeJaFezCheckIn } from '../service/corrida/checkInService';
-import { formatarDataCorrida, formatarHorarioCorrida } from '../service/corrida/corridaService';
-import { notificacaoGeral } from '../service/notificacaoGeral';
-import { Box, useToast } from 'native-base';
-import CategoriasDeCorridas from '../components/CategoriasDeCorridas';
-import { TEMAS } from "../style/temas";
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import Ionicons from "react-native-vector-icons/Ionicons";
-import { Image  } from "native-base";
-import { Cabecalho } from "../components/Cabecalho";
-import logoCKC1 from '../assets/logoCKC1.png';
+import React from "react";
+import { VStack, Text, Box } from "native-base";
+import {  useNavigation } from '@react-navigation/native';
+import { StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { confirmarLogoffENavegar } from "../service/perfil/perfilService";
+import { Cabecalho } from "../components/Cabecalho";
+import perfil from "../assets/perfil.jpg";
+import { TEMAS } from "../style/temas";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+
+
 
 export default function Perfil() {
   const navigation = useNavigation();
   
   return (
-    <VStack>
-      <View style={styles.background}>
-      <Cabecalho>
-      <Image style={styles.logo} source={logoCKC1} alt="Logo CKC"/>
-        </Cabecalho>
-        <Ionicons style={styles.iconPerfil} name="person-circle-outline"></Ionicons>
-      <View style={styles.container}>
-        <Text style={{fontFamily: TEMAS.fonts['petch_Bold'], fontSize: 20, alignSelf:'center'}}> Perfil    </Text>
-      </View>
+    <VStack style={styles.view}>
+      <Cabecalho key="cabecalho">
+        <Image style={styles.foto} source={perfil} alt="piloto de kart" />
+      </Cabecalho>
+      <Text style={styles.titulo} fontSize="5xl">olá, Thiago!</Text>
+
+      <Box style={styles.box}>
+        <Text style={styles.texto}>Vá até Check-in e escolha uma corrida para começar!</Text>
+        <Box style={styles.box_icone}>
+          <Ionicons style={styles.icone1} name="rocket-outline"></Ionicons>
+          <Ionicons style={styles.icone_check} name="checkmark-outline"></Ionicons>
+        </Box>
+      </Box>
+
+      <Box style={styles.box}>
+        <Text style={styles.texto}>Você já fez o Check-in? Então agora vá para Sortear os kart e boa sorte!</Text>
+        <Box style={styles.box_icone}>
+          <Ionicons style={styles.icone2} name="dice-outline"></Ionicons>
+          <Ionicons style={styles.icone_check} name="checkmark-outline"></Ionicons>
+        </Box>
+      </Box>
+
+      <Box style={styles.box}>
+        <Text style={styles.texto}>A corrida acabou? Então vá para Check-out para finalizar e já pensar na próxima!</Text>
+        <Box style={styles.box_icone}>
+          <Ionicons style={styles.icone3} name="trophy-outline"></Ionicons>
+          <Ionicons style={styles.icone_check} name="checkmark-outline"></Ionicons>
+        </Box>
+      </Box>
+
+      <Text style={styles.texto}>Ou click no botão para sair.</Text>
+
       <TouchableOpacity style={styles.botao} onPress={() => confirmarLogoffENavegar(navigation)}>
-        <Text style={styles.textoBotao}>
-          Log out
-        </Text>
-        <Ionicons style={styles.iconSair} name="log-in-outline"></Ionicons>
+        <Text style={styles.textoBotao}>Sair</Text>
       </TouchableOpacity>
-      </View>
     </VStack>
   );
 }
 
 const styles = StyleSheet.create({
-  
-
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontFamily: TEMAS.fonts['petch_Bold'],
-    paddingLeft: 20,
-    top: -50,
-  },
-
-  background: {
+  view: {
+    flex: 1,
     backgroundColor: TEMAS.colors.gray[300],
-    height:850,
+  },
+
+  foto: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    borderWidth: 2,
+    borderColor: TEMAS.colors.gray[300],
+  },
+
+  titulo: {
     fontFamily: TEMAS.fonts['petch_Bold'],
+    color: TEMAS.colors.black[300],
+    textAlign: "center",
+    marginTop: 60,
   },
 
-  logo: {
-    width: 70,
-    resizeMode: "contain",
-    alignSelf:'flex-start',
-    top:-60,
-    paddingLeft: 6,
+  texto: {
+    fontFamily: TEMAS.fonts['body'],
+    fontSize: TEMAS.fontSizes.md,
+    textAlign: "center",
   },
 
-  title: {
-    fontSize: 100,
-    fontFamily: TEMAS.fonts['petch_Bold'],
-    color:'black',
-    top:30,
+  box: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
+    marginHorizontal: 20,
   },
 
-  iconPerfil: {
-    color: 'white',
-    padding:1,
-    fontSize:100,
-    top:-90,
-    alignSelf:'center',
+  box_icone: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
-  iconSair: {
-    color: 'white',
-    fontSize:30,
+  icone1: {
+    fontSize: TEMAS.fontSizes.xl,
+    color: TEMAS.colors.red[300],
+    marginLeft: 10,
+  },
+
+  icone2: {
+    fontSize: TEMAS.fontSizes.xl,
+    color: TEMAS.colors.green[300],
+    marginLeft: 10,
+  },
+
+  icone3: {
+    fontSize: TEMAS.fontSizes.xl,
+    color: TEMAS.colors.orange[300],
+    marginLeft: 10,
+  },
+
+  icone_check: {
+    fontSize: TEMAS.fontSizes.xl,
+    color: TEMAS.colors.black[300],
   },
 
   botao: {
-    fontWeight: 'bold',
-    marginTop: 10,
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 100,
+    marginVertical: 20,
     backgroundColor: TEMAS.colors.blue[500],
-    borderRadius: 10,
-    marginBottom: 20,
-    position: 'absolute', 
-    bottom: 30,
-    height:55,
-    width:200,
-    alignContent:'center',
-    justifyContent:'center',
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignSelf: 'center',
-
   },
 
-textoBotao:{
+  textoBotao: {
+    color: TEMAS.colors.white,
+    fontSize: TEMAS.fontSizes.md,
     fontFamily: TEMAS.fonts['petch_Bold'],
-    color:'white',
     textAlign: "center",
-    fontSize: 15,
-    padding:5
-},
+  },
+});
+  
 
-})
+  
